47489cb7dd4951aa97423a5e971a564619dac248
Luiz Kim <luiz.kim@controleonline.com>
2024-12-19 22:46:31 -0300

    Update

---
+++ src/pages/Delivery/Details.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page padding>
+    <!--<InvoiceDetails />-->
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "InvoiceDetailsPage",
+
+  components: {
+    //InvoiceDetails,
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
