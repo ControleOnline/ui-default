b44ed49b07419a866987ea5af2bc99485d28e483
Luiz Kim <luiz.kim@controleonline.com>
2024-12-19 23:15:18 -0300

    Update

--- src/store/support/index.js
+++ src/store/support/index.js
@@ -4,17 +4,10 @@ import mutations from './mutations';
 
 export default {
   namespaced: true,
-<<<<<<<< HEAD:src/store/guides/index.js
-  getters,
-  mutations,
-  actions,
-  state,
-========
   state     : {
     isLoading: false,
   },
   actions,
   getters,
   mutations,
->>>>>>>> 6674ca873f77206b0d52470e7daeef418992ed67:src/store/support/index.js
 };
