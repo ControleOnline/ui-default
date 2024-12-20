3f6fb12bac4354b8210940435412800917d7b745
Luiz Kim <luiz.kim@controleonline.com>
2024-12-19 23:17:06 -0300

    Update

---
+++ src/pages/Import/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <CarrierImports />
+</template>
+
+<script>
+ import CarrierImports from "../../components/Import/ImportsList";
+export default {
+  components: {
+    CarrierImports,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
