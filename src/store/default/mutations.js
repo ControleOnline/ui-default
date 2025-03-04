6df52493e11e73696e1f22cac53eb9a20129ce24
Luiz Kim <luiz.kim@controleonline.com>
2025-03-04 01:35:08 -0300

    Update

--- src/store/default/mutations.js
+++ src/store/default/mutations.js
@@ -7,39 +7,39 @@ export default {
   },
 
   [types.SET_ISLOADING](state, isLoading = true) {
-    state.isLoading = isLoading;
+    state.isLoading = isLoading || false;
   },
 
   [types.SET_ISSAVING](state, isSaving = true) {
-    state.isSaving = isSaving;
+    state.isSaving = isSaving || false;
   },
 
   [types.SET_ISLOADINGLIST](state, isLoadingList = true) {
-    state.isLoadingList = isLoadingList;
+    state.isLoadingList = isLoadingList || false;
   },
 
   [types.SET_VIOLATIONS](state, violations) {
-    state.violations = violations;
+    state.violations = violations || null;
   },
 
   [types.SET_TOTALITEMS](state, totalItems) {
-    state.totalItems = totalItems;
+    state.totalItems = totalItems || 0;
   },
 
   [types.SET_ITEMS](state, items) {
-    state.items = items;
+    state.items = items || [];
   },
 
   [types.SET_ITEM](state, item) {
-    state.item = item;
+    state.item = item || {};
   },
 
   [types.SET_COLUMNS](state, columns) {
-    state.columns = columns;
+    state.columns = columns || [];
   },
 
   [types.SET_STORE](state, store) {
-    state.store = store;
+    state.store = store || null;
   },
 
   [types.SET_FILTERS](state, filters) {
@@ -48,23 +48,23 @@ export default {
       state.store || state.resourceEndpoint
     );
     persistentFilter.setFilters(filters);
-    state.filters = filters;
+    state.filters = filters || {};
   },
 
   [types.SET_RESOURCE_ENDPOINT](state, resourceEndpoint) {
-    state.resourceEndpoint = resourceEndpoint;
+    state.resourceEndpoint = resourceEndpoint || null;
   },
 
   [types.SET_SELECTED](state, selected) {
-    state.selected = selected;
+    state.selected = selected || [];
   },
 
   [types.SET_RELOAD](state, reload) {
-    state.reload = reload;
+    state.reload = reload || false;
   },
 
   [types.SET_VISIBLECOLUMNS](state, visibleColumns) {
-    state.visibleColumns = visibleColumns;
+    state.visibleColumns = visibleColumns || [];
     const persistentFilter = new Filters(
       state.$router?.currentRoute?.value?.name,
       state.store || state.resourceEndpoint
